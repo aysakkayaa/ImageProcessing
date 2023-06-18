1148fe1e6541be87fa4969b1b89063afdaa6283d
Aysegul Akkaya <90008953+aysakkayaa@users.noreply.github.com>
2023-06-18 13:36:44 +0300

    Update code.vhd

--- code.vhd
+++ code.vhd
@@ -13,19 +13,19 @@ file: out  std_logic_vector(15 downto 0);
 end sobel_filter;
 
 architecture structural of sobel_filter is
--- Binary ifadeleri okuma ve rame'ye yazma işlemi için gerekli veri tipi tanımlama
+-- Define the required data type for reading binary expressions and writing to rame
 type pixel_t  array (0 to 15) of std_logic_vector(15 downto 0);
 type frame_t is array (0 to 255) of std_logic_vector (15 downto 0);
 
--- Sobel matrisi uygulama işlemi için gerekli veri tipi tanımlama
+--Required data type definition for Sobel matrix application process
 type output_t is array (0 to 255) of std_logic_vector(15 downto 0);
 
--- Binary ifadeleri okuma işlemi için gerekli değişken tanımlama
+--Required variable definition for reading binary expressions
 variable file : text;
 variable pixel : pixel_t;
 variable input_line : line;
 
--- Sobel matrisi uygulama işlemi için gerekli değişken tanımlama
+-- Required variable definition for Sobel matrix application process
 constant sobel_h : array (0 to 1, 0 to 2) of integer := ((-1, 0, 1), (-2, 0, 2));
 constant sobel_v : array (0 to 2, 0 to 1) of integer := ((-1, -2), (0, 0), (1, 2));
 variable frame : frame_t;
@@ -36,18 +36,18 @@ begin
 process (clk, rst)
 begin
 if rst = '1' then
--- Reset işlemi
+-- Reset process
 file := null;
 pixel := (others => (others => '0'));
 frame := (others => (others => (others => '0')));
 output := (others => (others => (others => '0')));
 elsif rising_edge(clk) then
--- Txt dosyasını açma işlemi
+-- open txt file
 file := new text;
-file_open(file, download_jpg.txt, read_mode);
+file_open(file, exampleImageBinaryFormat.txt, read_mode);
 
 
--- Txt dosyasını okuma işlemi
+-- read txt file
 while not endfile(file) loop
 -- Her satırdaki pikselleri okuma
 for i in pixel 'range loop
@@ -57,7 +57,7 @@ end loop;
 
 
 
--- ram'e yazma
+-- write to RAM
 for row in 0 to 255 loop
   for col in 0 to 15 loop
     frame(row)(col) := pixel(row)(col);
@@ -65,34 +65,34 @@ for row in 0 to 255 loop
 end loop;
 end loop;
 
--- Txt dosyasını kapatma işlemi
+-- close txt file
 file_close(file);
 
--- Sobel matrisi uygulama işlemi
+-- Sobel matrix application process
 for row in 1 to 254 loop
   for col in 1 to 14 loop
--- Horizontal çıkış hesaplama
+-- Horizontal output calculation
 output(row)(col)(0) := frame(row-1)(col-1)(0) * sobel_h(0)(0) + frame(row-1)(col)(0) * sobel_h(0)(1) + frame(row-1)(col+1)(0) * sobel_h(0)(2) +
 frame(row)(col-1)(0) * sobel_h(1)(0) + frame(row)(col)(0) * sobel_h(1)(1) + frame(row)(col+1)(0) * sobel_h(1)(2);
--- Vertical çıkış hesaplama
+-- Vertical output calc.
 output(row)(col)(1) := frame(row-1)(col-1)(1) * sobel_v(0)(0) + frame(row-1)(col)(1) * sobel_v(0)(1) +
 frame(row)(col-1)(1) * sobel_v(1)(0) + frame(row)(col)(1) * sobel_v(1)(1) +
 frame(row+1)(col-1)(1) * sobel_v(2)(0) + frame(row+1)(col)(1) * sobel_v(2)(1);
 end loop;
 end loop;
 
--- Txt dosyasına yazma işlemi
+-- write to txt
 file := new text;
 file_open(file, "cıkısDosyasi.txt", write_mode);
 
--- Çıkışı txt dosyasına yazma
+--write output to txt file 
 for row in 0 to 255 loop
 for col in 0 to 15 loop
 write(file, output(row)(col));
 end loop;
 end loop;
 
--- Txt dosyasını kapatma işlemi
+-- close txt file
 file_close(file);
 
 end process;
